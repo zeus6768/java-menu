871575fe7a9ffef1ee0e28c5739f23df40436ee6
Zeus6768 <akaps6768@gmail.com>
2023-12-12 15:29:39 +0900

    feat: 코치 이름 입력 기능 예외 처리
    이름이 2글자 미만, 4글자 초과인 경우 예외 발생

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -8,16 +8,32 @@ import java.util.Set;
 
 public class Coach {
 
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final int MAX_NAME_LENGTH = 4;
+
     private final String name;
     private final Set<Menu> dislikedMenus;
 
     private Coach(String name) {
+        validate(name);
         this.name = name;
         dislikedMenus = new HashSet<>();
     }
 
     public static Coach from(String name) {
         return new Coach(name);
+    }
+
+    private void validate(String name) {
+        int length = name.length();
+        if (length < MIN_NAME_LENGTH || length > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 최소 "
+                    + MIN_NAME_LENGTH
+                    + "글자, 최대 "
+                    + MAX_NAME_LENGTH
+                    + "글자입니다."
+            );
+        }
     }
 
     public void addDislikedMenus(Collection<Menu> menus) {
